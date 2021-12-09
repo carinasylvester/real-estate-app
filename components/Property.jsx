80b274b904abfef1b5d398436f6f0f71e6d0dfcb
carinasylvester <carina.sylvester@outlook.com>
2021-12-08 20:03:38 -0500

    Property display

--- components/Property.jsx
+++ components/Property.jsx
@@ -2,11 +2,14 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { Box, Flex, Text } from '@chakra-ui/layout';
 import { Avatar } from '@chakra-ui/avatar';
+import millify from 'millify';
+
+// icons
 import { FaBed, FaBath } from 'react-icons/fa';
 import { BsGridFill } from 'react-icons/bs';
 import { GoVerified } from 'react-icons/go';
-import millify from 'millify';
 
+// images
 import DefaultImage from '../assets/images/house.jpg';
 
 const Property = ({ property: { coverPhoto, price, rentFrequency, rooms, title, baths, area, agency, isVerified, externalID  } }) => (
